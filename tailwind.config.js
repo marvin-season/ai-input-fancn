6d36ec35bd28ddceea67c6c37a72abc97672480c
marvin <sea-son@foxmail.com>
2025-02-14 13:46:17 +0800

    feat: base deps

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import lineClamp from '@tailwindcss/line-clamp'
+import typography from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: {
+  },
+  theme: {
+
+  },
+
+  plugins: [lineClamp, typography],
+}
