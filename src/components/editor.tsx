45aff9133e15c2c8c7a63af0b4f8f4df48b0c856
marvin <sea-son@foxmail.com>
2025-02-14 13:51:42 +0800

    feat: base editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,5 +1,24 @@
+import { EditorContent, useEditor } from '@tiptap/react'
+import { StarterKit } from '@tiptap/starter-kit'
+import { Markdown } from 'tiptap-markdown'
 export default function Editor() {
+    const editor = useEditor({
+        extensions: [
+            StarterKit.configure({
+                history: false,
+            }),
+            Markdown.configure({
+                html: true,
+                transformPastedText: true,
+                transformCopiedText: true,
+                breaks: true,
+                linkify: true,
+            }),
+        ],
+        content: `# hello world`,
+    })
+
     return <>
-Editor
+        <EditorContent className="w-full h-full  editorforce" editor={editor} />
     </>
 }
