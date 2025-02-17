4f571d39b87be6f2cc8b04ae6db166e3db127d0c
marvin <sea-son@foxmail.com>
2025-02-17 16:49:32 +0800

    feat: 使用上下文

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { Editor } from "./components";
-
+import { EditorProvider } from "@tiptap/react";
+import { useEditorProps } from "./components/editor";
 function App() {
-  return <div className="h-[100dvh] bg-[#f5f1f1] flex flex-col justify-center items-center">
-    <Editor/>
+  const editorProps = useEditorProps()
+  return <div className="h-[100dvh] bg-[#f5f1f1] flex flex-col justify-center items-center gap-4 p-8">
+    <EditorProvider editable={true} {...editorProps}>
+      <Editor/>
+    </EditorProvider>
   </div>;
 }
 
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,10 +1,13 @@
-import { EditorContent, useEditor } from '@tiptap/react'
+import { EditorProviderProps, useCurrentEditor } from '@tiptap/react'
 import { StarterKit } from '@tiptap/starter-kit'
 import { Markdown } from 'tiptap-markdown'
-// import Template from './extension/template'
 import InlinePlaceholder from './extension/inline-placeholder'
-export default function Editor() {
-    const editor = useEditor({
+
+export const useEditorProps = () => {
+    return {
+        editorContainerProps: {
+            className: 'w-full',
+        },
         autofocus: true,
         extensions: [
             StarterKit.configure({
@@ -23,30 +26,32 @@ export default function Editor() {
         content: `我是 <span data-type="inlinePlaceholder" data-placeholder="宫本武藏"></span> , 我的工作是 <span data-type="inlinePlaceholder" data-placeholder="吃饭睡觉打拳击"></span>`,
         editorProps: {
             attributes: {
-              class: 'prose-sm focus:outline-none',
+                class: 'w-full prose-sm focus:outline-none w-3/4 p-10 bg-white shadow rounded-[16px]',
             },
-            handleKeyDown: (view, event) => {
+            handleKeyDown: (_, event) => {
                 if (event.key === '@') {
-                  alert('trigger @')
-                  return true
+                    alert('trigger @')
+                    return true
                 }
                 // 监听键盘事件
                 if (event.key === 'Enter' && !event.shiftKey) {
                     console.log('trigger Enter')
-                  return true
+                    return true
                 }
-        
-                return false // 返回 false 不拦截事件
-              },
-        },
-    })
 
+                return false // 返回 false 不拦截事件
+            },
+        },
+    } satisfies EditorProviderProps
+}
+
+export default function Editor() {
+    const { editor } = useCurrentEditor()
     return <>
         <button className='py-2 px-3 rounded-lg bg-blue-400 text-white' onClick={() => {
             const json = editor?.getJSON();
             console.log('json', json)
             alert(`${JSON.stringify(json, null, 2)}`)
         }}>提交</button>
-        <EditorContent className="w-3/4 p-10 bg-white shadow rounded-[16px]" editor={editor} />
     </>
 }
