325e80f6f1a914d7ebe47eb778cd8413188ba7ab
marvin <sea-son@foxmail.com>
2025-02-17 11:21:14 +0800

    feat: serialize utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,35 +3,54 @@
 import type { JSONContent } from '@tiptap/core';
 
 export function deserialize(input: string) {
-  const result: JSONContent = {
-    type: 'doc',
-    content: [],
-  };
-  const regex = /{{(.*?)}}/g;
+  const content: JSONContent['content'] = [];
+  const regex = /{{(.*?)}}/g; // 匹配 {{...}} 占位符
   let lastIndex = 0;
+
   while (true) {
     const match = regex.exec(input);
     if (!match) {
+      // 如果没有更多匹配，添加剩余的普通文本
+      if (lastIndex < input.length) {
+        content.push({
+          type: 'text',
+          text: input.slice(lastIndex),
+        });
+      }
       break;
     }
 
     const [full, value] = match;
-    console.log('aa', full, value);
-    result.content?.push({
-      type: 'text',
-      text: input.slice(lastIndex, match.index),
-    });
-    result.content?.push({
+
+    // 添加占位符前的普通文本
+    if (lastIndex < match.index) {
+      content.push({
+        type: 'text',
+        text: input.slice(lastIndex, match.index),
+      });
+    }
+
+    // 添加占位符
+    content.push({
       type: 'inlinePlaceholder',
       attrs: {
         placeholder: value,
         value: '',
       },
     });
+
+    // 更新 lastIndex
     lastIndex = regex.lastIndex;
   }
 
-  return result;
+  return {
+    type: 'doc',
+    content: [
+      {
+        type: 'paragraph',
+        content,
+      },
+    ],
+  } satisfies JSONContent;
 }
-
 export const serialize = (str: string) => {};
